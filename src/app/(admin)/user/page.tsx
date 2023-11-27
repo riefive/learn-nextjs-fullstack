0902fca03e793665a7bdd2ad232ccb9aea6c3d14
riefive <rie.five@gmail.com>
2023-11-27 10:18:44 +0700

    chore: add page user

---
+++ src/app/(admin)/user/page.tsx
@@ -0,0 +1,8 @@
+import { userGetAll } from "@/services/user.service"
+
+export default function PageAdminUser() {
+    userGetAll().then((result: any) => {
+        console.log(result)
+    })
+    return <h1>User page in Next.js!</h1>
+}
